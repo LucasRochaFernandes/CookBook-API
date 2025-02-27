6d6dc40d65aea537c185db9f48baaecb2315df9d
Lucas Rocha Fernandes <lucasrochaf2018@gmail.com>
2025-02-27 18:40:32 -0300

    feat: adding integration tests

--- RevenuesBook.IntegrationTests/CustomWebApplicationFactory.cs
+++ RevenuesBook.IntegrationTests/CustomWebApplicationFactory.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Hosting;
+﻿using CommonTestUtilities.Entities;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
@@ -7,6 +8,8 @@ using RevenuesBook.Infra;
 namespace RevenuesBook.IntegrationTests;
 public class CustomWebApplicationFactory : WebApplicationFactory<Program>
 {
+    private Domain.Entities.User _user = default!;
+    private string _password = string.Empty;
     protected override void ConfigureWebHost(IWebHostBuilder builder)
     {
         builder.UseEnvironment("Test")
@@ -17,11 +20,38 @@ public class CustomWebApplicationFactory : WebApplicationFactory<Program>
                     services.Remove(descriptor);
 
                 var provider = services.AddEntityFrameworkInMemoryDatabase().BuildServiceProvider();
+
+
                 services.AddDbContext<AppDbContext>(options =>
                 {
                     options.UseInMemoryDatabase("InMemoryDbForTesting");
                     options.UseInternalServiceProvider(provider);
                 });
+
+                using var scope = services.BuildServiceProvider().CreateScope();
+
+                var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+
+                dbContext.Database.EnsureDeleted();
+
+                StartDatabase(dbContext);
             });
+    }
+
+    public string GetUserDefaultEmail()
+    {
+        return _user.Email;
+    }
+    public string GetUserDefaultPassword()
+    {
+        return _password;
+    }
+    private void StartDatabase(AppDbContext dbContext)
+    {
+        (_user, _password) = UserBuilder.Build();
+
+        dbContext.Users.Add(_user);
+
+        dbContext.SaveChanges();
     }
 }
